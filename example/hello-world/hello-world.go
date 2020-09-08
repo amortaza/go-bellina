e9564ab1e295d6f854a834fb05f206f2c88cfa72
amortaza <emailalpha3@gmail.com>
2020-09-07 21:10:47 -0700

    .

--- example/hello-world/hello-world.go
+++ example/hello-world/hello-world.go
@@ -28,7 +28,7 @@ func init() {
 }
 
 func main() {
-	bl.Start( hal_g5.NewHal(), "Bellina v0.2", 1280, 1024, initialize, tick, uninit )
+	bl.Start( hal_g5.NewHal(), "Bellina v0.2", 10,10,1280, 1024, initialize, tick, uninit )
 
 	fmt.Println("bye!")
 }
